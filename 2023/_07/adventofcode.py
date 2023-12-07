6746ebf0a7c07066d1cb9d1946c20e0b7e577dcb
phvande2001 <philip.vandendonk@nielsen.com>
2023-12-07 14:28:29 +0100

    day 7 cleanup

--- 2023/_07/adventofcode.py
+++ 2023/_07/adventofcode.py
@@ -10,45 +10,63 @@ day = os.getcwd().split('_')[-2]
 with open(file, 'r') as f:
     data = [txt.replace('\n', '') for txt in f.readlines()]
 
-##a
 cards = [x.split()[0] for x in data]
 bids = [int(x.split()[1]) for x in data]
-vals = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}
 
-def no_joker(cards, bids):
-    vals = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}
+
+def play_game(cards, bids, joker=False):
+    # determine sort values
+    if not joker:
+        vals = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}
+    else:
+        vals = {'A': 14, 'K': 13, 'Q': 12, 'J': 1, 'T': 10}
+
+    # define game as a dataframe
     hands = pd.DataFrame({'cards': cards, 'bid': bids})
-    hands['strength'] = [''.join([str(x) for x in sorted(list(Counter(hand).values()))[::-1]]) for hand in cards]
+
+    # determine hand strengths
+    hands['strength'] = hand_strength(cards, joker=joker)
+
+    # determine order of cards by numerical values
     hands['values'] = hands['cards'].apply(lambda c: [vals[x] if x in vals else int(x) for x in c])
     values_order = sorted(hands['values'])
     hands['order'] = hands['values'].apply(lambda x: values_order.index(x))
+
+    # order by hand strength, then by numerical order in case of tiebreakers
+    # possible hand strengths 5, 41, 32, 311, 221, 2111, 1111 already follow order of actual strengths
     hands = hands.sort_values(['strength', 'order'], ascending=True).reset_index(drop=True)
+
+    # rank based on order and determine winnings
     hands['rank'] = hands.index + 1
     hands['winnings'] = hands['bid'] * hands['rank']
 
     return hands
 
-ans_a = no_joker(cards, bids)['winnings'].sum()
+
+def hand_strength(cards, joker=False):
+    # count initial cards in hand
+    counter = [Counter(hand) for hand in cards]
+
+    if joker:
+        # replace J by most common card (unless most common card is J and J is not the only card)
+        # else replace J by second most common card
+        counter = [Counter(hand) for hand in
+                   [c.replace('J',
+                              counts.most_common()[0][0] if not counts.most_common()[0][0] == 'J' or len(counts) == 1
+                              else
+                              counts.most_common()[1][0])
+                    for c, counts in zip(cards, counter)]]
+
+    # convert to str: possible hand strengths 5, 41, 32, 311, 221, 2111, 1111
+    strength = [''.join([str(x) for x in sorted(list(counts.values()))[::-1]]) for counts in counter]
+
+    return strength
+
+##a
+ans_a = play_game(cards, bids, joker=False)['winnings'].sum()
 
 ##b
-def joker(cards, bids):
-    vals = {'A': 14, 'K': 13, 'Q': 12, 'J': 1, 'T': 10}
-    hands = pd.DataFrame({'cards': cards, 'bid': bids})
-
-    hands['counter'] = [Counter(hand) for hand in cards]
-    hands['strength'] = [''.join([str(x) for x in sorted(list(Counter(hand).values()))[::-1]])
-                         for hand in [c.replace('J', x.most_common()[1][0] if len(x.most_common()) > 1 and x.most_common()[0][0] == 'J' else x.most_common()[0][0])
-                                      for c, x in zip(hands['cards'], hands['counter'])]]
-    hands['values'] = hands['cards'].apply(lambda c: [vals[x] if x in vals else int(x) for x in c])
-    values_order = sorted(hands['values'])
-    hands['order'] = hands['values'].apply(lambda x: values_order.index(x))
-    hands = hands.sort_values(['strength', 'order'], ascending=True).reset_index(drop=True)
-    hands['rank'] = hands.index + 1
-    hands['winnings'] = hands['bid'] * hands['rank']
-
-    return hands
-
-ans_b = joker(cards, bids)['winnings'].sum()
+ans_b = play_game(cards, bids, joker=True)['winnings'].sum()
 
 if __name__ == "__main__":
     print(f'Answer {day}a:', ans_a)
